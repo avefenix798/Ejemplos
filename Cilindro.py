d5883d8f8a095ffd5004fc55eae0337a6e43b70d
Emmanuel Salinas Coyote <avefenix798@hotmail.com>
2020-06-27 15:32:50 -0500

    Create Cilindro.py
    Codigo cilindro

---
+++ Cilindro.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 15:30:13 2020
+
+@author: Emmanuel
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure(figsize = (10,8))
+ax = fig.add_subplot(111, projection='3d')
+
+# grid del cilindro
+x=np.linspace(-1, 1, 200)
+z=np.linspace(-2, 2, 200)
+Xc, Zc=np.meshgrid(x, z)
+Yc = np.sqrt(1-Xc**2)
+
+
+# Plot del cilindro en dos partes
+ax.plot_surface(Xc, Yc, Zc, alpha=0.2, rstride=10, cstride=10) # Mitad delantera
+ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=10, cstride=10) # Mitad trasera
+
+plt.show()
